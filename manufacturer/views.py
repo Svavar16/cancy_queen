2b82265b6ecaafd5ccd61f4fe43cdd2c4af9a8b9
Svavar16 <spg009@gmail.com>
2019-05-05 18:47:32 +0000

    added manufacturer single view

--- manufacturer/views.py
+++ manufacturer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from manufacturer.models import Manufacturer
 # Create your views here.
 
@@ -6,4 +6,10 @@ from manufacturer.models import Manufacturer
 def index(request):
     return render(request, 'manufacturer/index.html', {
         'manufacturer': Manufacturer.objects.all()
+    })
+
+
+def get_manufacturer_by_id(request, id):
+    return render(request, 'manufacturer/manufacturer_details.html', {
+        'manufacturer': get_object_or_404(Manufacturer, pk=id)
     })
