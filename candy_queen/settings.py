ab680342907701d7ee41186f1b7d74322b97fd20
Svavar16 <spg009@gmail.com>
2019-05-05 17:10:29 +0000

    Adding database connection

--- candy_queen/settings.py
+++ candy_queen/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'candy_queen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yjjsdwll',
+        'USER': 'yjjsdwll',
+        'PASSWORD': 'aXKxCtJ6HLjK41getgAaAMudNM3ZRFr4',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
