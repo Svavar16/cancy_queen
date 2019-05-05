11c3eadbe42e372171a0d1f23652cc1477f5d721
Svavar16 <spg009@gmail.com>
2019-05-05 18:39:35 +0000

    added candy single details

--- candy/views.py
+++ candy/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from candy.models import Candy
 
 # Create your views here.
@@ -7,3 +7,9 @@ from candy.models import Candy
 def index(request):
     context = {'candies': Candy.objects.all().order_by('name')}
     return render(request, 'candy/index.html', context)
+
+
+def get_candy_by_id(request, id):
+    render(request, 'candy/candy_details.html', {
+        'candy': get_object_or_404(Candy, pk=id)
+    })
