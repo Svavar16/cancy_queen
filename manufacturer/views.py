9669af5187784167519e7f87b1e2910210b34e21
Svavar16 <spg009@gmail.com>
2019-05-05 18:30:42 +0000

    added manufacturer views

--- manufacturer/views.py
+++ manufacturer/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from manufacturer.models import Manufacturer
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'manufacturer/index.html')
+    return render(request, 'manufacturer/index.html', {
+        'manufacturer': Manufacturer.objects.all()
+    })
