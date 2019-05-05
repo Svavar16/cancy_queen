a50610108696011f3e9db23e9cfd3544f6d83bcd
Svavar16 <spg009@gmail.com>
2019-05-05 20:22:00 +0000

    added update candy

--- candy/views.py
+++ candy/views.py
@@ -1,11 +1,22 @@
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from candy.models import Candy, CandyImage
-from candy.forms.candy_form import CandyCreateForm
+from candy.forms.candy_form import CandyCreateForm, CandyUpdateForm
+
 
 # Create your views here.
 
 
 def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        candies = [{
+            'id': x.id,
+            'name': x.name,
+            'description': x.description,
+            'firstImage': x.candyimage_set.first().image
+        } for x in Candy.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': candies})
     context = {'candies': Candy.objects.all().order_by('name')}
     return render(request, 'candy/index.html', context)
 
@@ -23,9 +34,30 @@ def create_candy(request):
             candy = form.save()
             candy_image = CandyImage(image=request.POST['image'], candy=candy)
             candy_image.save()
-            return redirect()
+            return redirect('candy-index')
     else:
         form = CandyCreateForm()
     return render(request, 'candy/create_candy.html', {
         'form': form
+    })
+
+
+def delete_candy(request, id):
+    candy = get_object_or_404(Candy, pk=id)
+    candy.delete()
+    return redirect('candy-index')
+
+
+def update_candy(request, id):
+    instance = get_object_or_404(Candy, id)
+    if request.method == "POST":
+        form = CandyUpdateForm(data=request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return render('candy_detail', id=id)
+    else:
+        form = CandyUpdateForm(instance=instance)
+    return render(request, 'candy/update_candy.html',{
+        'form': form,
+        'id': id
     })
