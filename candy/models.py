ce91b048d44fe150469cc0a02147831be46881c3
Svavar16 <spg009@gmail.com>
2019-05-05 17:55:03 +0000

    added candy model, manufacturer model

--- candy/models.py
+++ candy/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from manufacturer.models import Manufacturer
 # Create your models here.
+
+
+class CandyCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Candy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=999, blank=True) #blank means that it isnt required.
+    category = models.ForeignKey(CandyCategory, on_delete=models.CASCADE)
+    price = models.FloatField()
+    on_sale = models.BooleanField()
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+
+class CandyImage(models.Model):
+    image = models.CharField(max_length=999)
+    candy = models.ForeignKey(Candy, on_delete=models.CASCADE)
